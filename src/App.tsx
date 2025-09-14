203cf142e7465940a520bd7e95b877ced70ab51d
Edudje wisdom marvellous <163600836+Citycod@users.noreply.github.com>
2025-09-14 11:37:20 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
-import { Navbar } from "./components/Navbar/Navbar";
-import "./index.css";
-import FarmingInnovationSection from "./components/FarmingInnovationSection";
-import HeroSection from "./components/HeroSection";
-import AboutSection from "./components/AboutSection";
-import ContactForm from "./components/ContactForm";
-import Prompt from "./components/prompt";
+import React from 'react';
+import FAQAccordion from './components/FAQAccordion';
 
-const HomePage: React.FC = () => {
-  const navigate = useNavigate();
-
-  const handleJoinClick = () => {
-    navigate("/Prompt");
-  };
-
+function App() {
   return (
-    <>
-      <Navbar onJoinClick={handleJoinClick} />
-      <HeroSection />
-      <FarmingInnovationSection />
-      <AboutSection />
-      <ContactForm />
-    </>
+    <div className="app">
+      
+        <FAQAccordion />
+  
+    </div>
   );
-};
-
-const App: React.FC = () => {
-  return (
-    <Router>
-      <div className="min-h-screen border-4 border-green-400 sm:px-8 lg:px-10">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Prompt" element={<><Navbar onJoinClick={() => {}} /><Prompt /></>} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+}
 
 export default App;
